54b4f84ae0478b032c756423cf5c7ccd31c0fe53
Charlekson <charlekson7@gmail.com>
2025-10-25 10:48:49 +0700

    car.java

--- car.java
+++ car.java
@@ -7,7 +7,6 @@ public class Car {
     private double price;
     private String registrationNumber;
 
-    // Конструктор
     public Car(int id, String brand, String model, int year, String color, double price, String registrationNumber) {
         this.id = id;
         this.brand = brand;
@@ -18,7 +17,6 @@ public class Car {
         this.registrationNumber = registrationNumber;
     }
 
-    // Геттеры и сеттеры
     public int getId() {
         return id;
     }
